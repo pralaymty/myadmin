5ffe81178639417b563cc791a9e0ee00915c04a2
Pralay Maity <pralay.mty@gmail.com>
2020-01-14 15:26:55 +0530

    ha

--- function.php
+++ function.php
@@ -10,26 +10,25 @@ class Connect
     private $dbname = null;
     private $username = null;
     private $password = null;
-        function __construct($host,$dbname,$username,$password)
-        {
-            $this->host = $host;
-            $this->dbname = $dbname;
-            $this->username = $username;
-            $this->password = $password;
-            $host = "localhost";
-            $dbname = "project";
-            $username = "root";
-            $password = "";
-        }
-        
-        function connect(){
-        try {
-        $conn = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname, $this->username, $this->password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch(PDOException $e) {
-        echo 'Unable to connect';
-        }
-        }
+    function __construct($host,$dbname,$username,$password)
+    {
+        $this->host = $host;
+        $this->dbname = $dbname;
+        $this->username = $username;
+        $this->password = $password;
+        $host = "localhost";
+        $dbname = "project";
+        $username = "root";
+        $password = "";
+    }
+    function connect(){
+    try {
+    $conn = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname, $this->username, $this->password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+    echo 'Unable to connect';
+    }
+    }
 } 
 $con = new Connect();
 $con->connect();
