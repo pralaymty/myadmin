e90d3f36a4ae9a213cf421fcd7c1a1e5aaa81ba6
Pralay Maity <pralay.mty@gmail.com>
2020-01-31 18:23:30 +0530

    com

--- functions1.php
+++ functions1.php
@@ -1,29 +1,26 @@
 <?php
 
 class dao {
-
         public function __construct() {
-
             try {
                 $dbh = new PDO("mysql:host=localhost;dbname=project", 'root', '');
             } catch (PDOException $e) {
                 print "Error!: " . $e->getMessage() . "<br />";
                 die();
             }
-
             $this->dbh = $dbh;
-
         }
 
         public function getCities() {
-                $stmt= $this->dbh->prepare("INSERT INTO users ( name, email, password) VALUES (1, 2, 3)");
+                $stmt= $this->dbh->prepare("INSERT INTO users ( name, email, password) VALUES (11, 33, 551)");
                 $stmt->execute();
-                $result = $stmt->fetchAll();
-                return $result;
-                }
+                //$result = $stmt->fetchAll();
+                //return $result;
+        }
 }
 
 $dbObj = new dao();
 $cities = $dbObj->getCities();
 
+//print_r ($cities);
 ?>
